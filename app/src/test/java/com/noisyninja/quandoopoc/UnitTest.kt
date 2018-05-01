fbc2d595ab1bcc0bd5cc90b75e97fe387b47f40f
sudiptadutta <ir2pid@gmail.com>
2018-05-01 22:10:32 +0200

    Ninth Commit added test

---
+++ app/src/test/java/com/noisyninja/quandoopoc/UnitTest.kt
@@ -0,0 +1,33 @@
+package com.noisyninja.quandoopoc
+
+import com.noisyninja.quandoopoc.layers.Utils
+import com.noisyninja.quandoopoc.model.Table
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class UnitTest {
+
+    lateinit var table: Table
+    @Before
+    fun setup() {
+        table = Table(true, 100)
+    }
+
+    @Test
+    fun marshall() {
+        var json = Utils.toJson(table)
+        Assert.assertNotNull(json)
+    }
+
+    @Test
+    fun unmarshall() {
+        var json = Utils.toJson(table)
+        Assert.assertEquals(table, Utils.fromJson(json, Table::class.java))
+    }
+}
